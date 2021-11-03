837d48a00f560f94f0af3ae7e90ffe3077b313d2
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-03 15:47:04 +0100

    Finish title

---
+++ src/app/components/Title/Title.tsx
@@ -0,0 +1,13 @@
+// component that returns a h1 and displays a title
+// style it to your liking
+import React from 'react';
+import type { ReactNode } from 'react';
+import styles from './Title.module.css';
+
+type TitleProps = { children: ReactNode };
+
+function Title({ children }: TitleProps): JSX.Element {
+  return <h1 className={styles.title}>{children}</h1>;
+}
+
+export default Title
