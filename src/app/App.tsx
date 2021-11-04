9a2f5ed65dd0d17e2ede894d41e1b2c1cc2e0dd7
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-04 16:23:26 +0100

    Implement renderCards

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDo from './components/ToDo/ToDo';
 import Title from './components/Title/Title';
 import Form from './components/Form/Form';
 
 function App(): JSX.Element {
-  function handleSubmit(todo) {
-    const title = todo.title;
-    const description = todo.description;
+  const [todos, setTodos] = useState([
+    { isDone: false, title: 'Einkaufen', description: 'Hafermilch' },
+    { isDone: true, title: 'Schuhe putzen', description: 'Schuhschrank' },
+    { isDone: false, title: 'Saiten wechseln', description: 'Ibanez GRG170DX' },
+  ]);
 
-    console.log(title);
-    console.log(description);
+  function handleSubmit(todo: {
+    title: string;
+    description: string;
+    isDone: boolean;
+  }): void {
+  
+    const newTodos = todos.slice();
+    newTodos.push(todo);
+    setTodos(newTodos);
+  }
+
+  function renderCards(
+    todos: { title: string; description: string; isDone: boolean }[]
+  ) {
+    return todos.map((todo) => {
+      return (
+        <ToDo
+          key={todo.title}
+          title={todo.title}
+          description={todo.description}
+          isDone={todo.isDone}
+        />
+      );
+    });
   }
 
   return (
     <div>
       <Title>Hier steht der Titel</Title>
       <Form onSubmit={handleSubmit} />
-      <ToDo
-        isDone={false}
-        title={'Schuhe putzen'}
-        description={'Töte die Schuhe'}
-      ></ToDo>
+      {renderCards(todos)}
     </div>
   );
 }
--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import styles from './Form.module.css';
 
-function Form({ onSubmit }): JSX.Element {
+type FormProps = {
+  onSubmit: (todo: {
+    title: string;
+    description: string;
+    isDone: boolean;
+  }) => void;
+};
+
+function Form({ onSubmit }: FormProps): JSX.Element {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    onSubmit({ title, description });
+    onSubmit({ title, description, isDone: false });
   }
 
   return (
